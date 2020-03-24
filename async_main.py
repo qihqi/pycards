a5f1619fca032d4c47456b9d456f401cecfb9dfe
hangsheng <qihan.dev@gmail.com>
2020-03-23 22:44:37 -0700

    add bunch of stuff

---
+++ async_main.py
@@ -0,0 +1,100 @@
+import json
+import aiohttp
+import aiohttp_jinja2
+from aiohttp import web
+import jinja2
+
+from pycards import game
+
+# messages are tuples:
+# (DRAW, n)
+# (PLAY, list-of-cards)
+
+async def index(request):
+    ws_current = web.WebSocketResponse()
+    ws_ready = ws_current.can_prepare(request)
+
+    if not ws_ready.ok:
+        return aiohttp_jinja2.render_template('index.html', request, {})
+
+    # Does this means I have connected in js?
+    await ws_current.prepare(request)
+    g = request.app['games']
+    name = None
+
+    while True:
+        msg = await ws_current.receive()
+        broadcast_result = {}
+        cur_result = {}
+        if msg.type == aiohttp.WSMsgType.text:
+            try:
+                parsed = json.loads(msg.data)
+                action, arg = parsed['action'], parsed['arg']
+                if action == 'NEW_PLAYER':
+                    request.app['websockets'][arg] = ws_current
+                    g.new_player(arg)
+                    name = arg
+                    broadcast_result['players'] = g.players()
+                elif action == 'START':
+                    g.start(arg)
+                elif action == 'DRAW':
+                    g.draw(name, arg)
+                    cur_result['hand'] = g.get_hand(name)
+                elif action == 'CLEAN_TABLE':
+                    g.clean_table()
+                    broadcast_result['table'] = g.table()
+                elif action == 'PLAY':
+                    arg = map(int, arg)
+                    g.play(name, arg)
+                    broadcast_result['table'] = g.table()
+                    cur_result['hand'] = g.get_hand(name)
+                else:
+                    raise ValueError('{} is not valid action'.format(action))
+            except ValueError as e:
+                print(e)
+                import traceback
+                traceback.print_exc()
+                await ws_current.send_json({'error': str(e)})
+            else:
+                if broadcast_result:
+                    for ws in request.app['websockets'].values():
+                        await ws.send_json(broadcast_result)
+                if cur_result:
+                    await ws_current.send_json(cur_result)
+        else:
+            break
+
+    del request.app['websockets'][name]
+    g.remove_player(name)
+    for ws in request.app['websockets'].values():
+        await ws.send_json({'players': g.players() })
+
+    return ws_current
+
+
+
+async def init_app():
+    app = web.Application()
+    app['websockets'] = {}
+    app['games'] = game.Game()
+    app.on_shutdown.append(shutdown)
+    aiohttp_jinja2.setup(
+            app, loader=jinja2.FileSystemLoader('templates'))  # directories relative
+    app.router.add_get('/', index)
+    app.router.add_routes([web.static('/static', 'static')])
+    return app
+
+
+async def shutdown(app):
+    for ws in app['websockets'].values():
+        await ws.close()
+    app['websockets'].clear()
+
+
+def main():
+    app = init_app()
+    web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
--- pycards/game.py
+++ pycards/game.py
@@ -3,45 +3,62 @@ import random
 NUM_CARDS_PER_DECK = 54
 
 
+def make_standard_deck():
+    cards = []
+    for suite in ('C', 'D', 'H', 'S'):
+        for val in ['A'] + [str(i) for i in range(2, 11)] + ['J', 'Q', 'K']:
+            cards.append((suite, val))
+    cards.append(('JOKER', 'SMALL'))
+    cards.append(('JOKER', 'BIG'))
+    return cards
+
+STANDARD_DECK = make_standard_deck()
+
+def get_card(cid):
+    return {'id': cid, 'value': STANDARD_DECK[cid % 54]}
+
+
+
 class Game(object):
 
     def __init__(self, num_cards_per_deck=None):
         self._num_cards = num_cards_per_deck or NUM_CARDS_PER_DECK
         self.deck = []
-        self.status = 'NEW'
-        self._players = {}  # id -> player obj
         self._table = []
         self._player_hands = {}
 
-    def new_player(self, player):
-        if self.status != 'NEW':
-            raise ValueError('')
-        self._players[player.uid] = player
-        self._player_hands[player.uid] = []
+    def new_player(self, name):
+        self._player_hands[name] = set()
 
     def start(self, num_decks):
         self.deck = list(range(num_decks * self._num_cards))
         random.shuffle(self.deck)
-        self._player_hands = {pid: [] for pid in self._players}
 
     def draw(self, player_id, count):
         if count > len(self.deck):
             raise ValueError('draw to many')
         cards = self.deck[-count:]
-        self._player_hands[player_id].extend(cards)
+        self._player_hands[player_id].update(cards)
         del self.deck[-count:]
         return cards
 
-    def play(self, player_id, card_id):
-        self._player_hands[player_id].remove(card_id)
-        self._table.append(card_id)
+    def play(self, player_id, card_ids):
+        for c in card_ids:
+            print(c)
+            self._player_hands[player_id].remove(c)
+            self._table.append(c)
 
     def clean_table(self):
         self._table = []
 
-    def get_state(self, player_id):
-        return  {
-                'table': self._table,
-                'hand': self._player_hands[player_id]
-                }
+    def get_hand(self, player_id):
+        return list(map(get_card, self._player_hands[player_id]))
 
+    def table(self):
+        return list(map(get_card, self._table))
+
+    def players(self):
+        return list(self._player_hands.keys())
+
+    def remove_player(self, player):
+        del self._player_hands[player]
